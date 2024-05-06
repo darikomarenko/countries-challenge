b052cef8b5e43dc04dfe7990c7fd948f5fa7a8a9
Daria Komarenko <dari.komarenko@gmail.com>
2024-05-06 13:27:27 +0300

    Add PUBLIC_URL env to router for correct deploying on github

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
